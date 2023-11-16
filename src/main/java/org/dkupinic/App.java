d43430666144d72eca820587948ffbe0af1a2723
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-11-16 15:56:06 +0100

    add new functions for todo

--- src/main/java/org/dkupinic/App.java
+++ src/main/java/org/dkupinic/App.java
@@ -1,7 +1,98 @@
 package org.dkupinic;
 
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<TodoList> todoLists = new ArrayList<>();
+        TodoList todoList;
+        String operation = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Todo-List Manager");
+        showInformation();
+
+        while (!operation.equals("exit")) {
+            System.out.print("--> ");
+            operation = scanner.next();
+            if (scanner.hasNextLine()) {
+                switch (operation) {
+                    case "n" -> todoLists.add(createTodo());
+                    case "e" -> System.out.println("");
+                    case "d" -> delete(todoLists);
+                    case "h" -> showInformation();
+                    case "exit" -> System.out.println("Exiting program");
+                    case "p" -> printTodos(todoLists);
+                    default -> System.out.println("Invalid Information");
+                }
+            }
+            System.out.println("   [h] - show help information");
+        }
+    }
+
+    public static void showInformation() {
+        System.out.println("Enter: \n   [n] - new Todo-List \n" +
+                "   [e] - edit existing Todo-List \n" +
+                "   [d] - delete exisiting Todo-List \n" +
+                "   [exit] - exit program \n" +
+                "   [p] - print all Todo-Lists with Todos"
+        );
+    }
+
+    public static TodoList createTodo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Creating Todo");
+        System.out.print("Enter a Name for your Todo-List: ");
+
+        return new TodoList(new ArrayList<>(), scanner.nextLine());
+    }
+
+    public static void delete(ArrayList<TodoList> todoList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter: \n   [t] - delete an element of a Todo-List \n" +
+                "   [w] - delete whole Todo-List");
+        System.out.println("--> ");
+
+        switch (scanner.nextLine()) {
+            case "t" -> deleteTodo();
+            case "w" -> deleteTodoList(todoList);
+            default -> System.out.println("Invalid Information");
+        }
+    }
+
+    public static void deleteTodoList(ArrayList<TodoList> todoList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the Todo-List you would like to delete: ");
+        String listName = scanner.nextLine();
+        todoList.removeIf(t -> t.getName().equalsIgnoreCase(listName));
+    }
+
+    public static void deleteTodo() {
+    }
+
+    public static void printTodos(ArrayList<TodoList> todoList) {
+        for (TodoList t : todoList) {
+            System.out.println("Todo-List '" + t.getName() + "'");
+            for (Todo element : t.getTodos()) {
+                System.out.println("Todo: " + element.getTitle() + "'");
+            }
+        }
+    }
+
+    public static void editTodoList () {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scannerTodo = new Scanner(System.in);
+        System.out.println("Enter the name of the Todo-List you would like to edit: ");
+        String listName = scanner.nextLine();
+
+
+        System.out.println("[a] - Add new Todo");
+        String todoName = scannerTodo.next();
+
     }
 }
+
+
